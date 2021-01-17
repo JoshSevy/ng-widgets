65c8fbb95c19bc426a8745f8bf41e84fb3e460ed
JoshSevy <joshuasevy@outlook.com>
2021-01-17 15:37:34 -0700

    Add styling to start with only one item expanded from accordion

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
   @Input() items = [];
+  openedItemIndex = 0;
   constructor() { }
 
   ngOnInit(): void {
